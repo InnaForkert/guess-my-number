919e391f12653b07ee39f0ae01c4f480f6d200fd
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-11 21:44:58 +0200

    number input field

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <StartGameScreen />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,54 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+
+    borderRadius: 8,
+
+    backgroundColor: "#72063c",
+
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    marginVertical: 8,
+
+    fontSize: 32,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
+
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+  },
+});
+
+export default StartGameScreen;
